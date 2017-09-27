26e7f11b8329a86c3cecf0966e40a6fb4d52eff4
Jared Laufiso <jared.laufiso@gmail.com>
2017-09-27 16:42:36 -0600

    setting things up

---
+++ radix2_cooley-tookey_1D_FFT/main.c
@@ -0,0 +1,13 @@
+// This is my first attempt to implement the fft algorithm in opencl. 
+//
+//
+//
+
+#include "radix2_cooley-tookey_1D_FFT.h"
+
+int main(char* argc, int* argv){
+	
+
+
+
+}
---
+++ radix2_cooley-tookey_1D_FFT/radix2_cooley-tookey_1D_FFT.h
@@ -0,0 +1,10 @@
+
+//system includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> // I'm not sure if needed
+
+// OpenCL includes
+#include <CL/cl.h>
+
+
